e4e2ad3fb90e48118d0f1e311eb6818e4dfbd292
Athi2019rit <89637227+Athi2019rit@users.noreply.github.com>
2021-09-22 13:33:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>lunchin internship<h1>
+        <h1>lunchin internship</h1>
 
         <a
           className="App-link"
